7f81ae1f34d8db8b9c3711a6e43522da045876e6
Ujjwal Arora <ukarora9@gmail.com>
2025-02-15 14:55:51 +0530

    Initial Commit

---
+++ NetflixUIKitRevision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixUIKitRevision
+//
+//  Created by Ujjwal Arora on 15/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
